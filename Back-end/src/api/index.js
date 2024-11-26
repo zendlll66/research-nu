c88dc860dda1732c0bd24cf3be0661cb2651f572
ZEnd <om99305@gmail.com>
2024-11-26 23:00:26 +0700

    test api

--- Back-end/src/api/index.js
+++ Back-end/src/api/index.js
@@ -10,6 +10,12 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/hi', (req, res) => {
+  res.json({
+    message: 'API - 👋',
+  });
+});
+
 router.use('/emojis', emojis);
 
 module.exports = router;
